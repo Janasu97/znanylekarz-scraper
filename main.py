c4f39545951be866a411534dd0a71f9cd5021f6c
Janasu97 <62239594+Janasu97@users.noreply.github.com>
2023-07-11 00:17:05 +0200

    Now the program will also scratch the avatar image of the doctors. For now it saves it under one one name. In future will be saved in the SQL data base as well as all of the scratched data.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import matplotlib.image as mpimg
 docnamearr = []
 streetnamearr = []
 citynamearr = []
-docspec = ["stomatolog"]
+docspec = ["stomatolog", "ginekolog", "ortopeda", "psycholog", "chirurg", "dermatolog", "fizjoterapeuta"]
 
 hold_url = "https://www.znanylekarz.pl/%s" % docspec[0]
 url = hold_url
 
-for i in range(0, 21): #The range can go up to 500. There is no url code after that
+for i in range(0, 5): #The range can go up to 500. There is no url code after that
     if i > 0:
         url = hold_url + "/" + str(i)
 
@@ -32,27 +32,31 @@ for i in range(0, 21): #The range can go up to 500. There is no url code after t
             hold_address = doc.find("span", class_="text-truncate").get_text()
             address_split = hold_address.split(", ") #Split address to the street and city name
 
-            # Get the avatar of the doctor
-            # image = doc.find("a", class_="img")
-            # hold_image = image.src
-            image = doc.select('div img')
-            image_url = image[0]['src']
-            img_data = requests.get("https://www.znanylekarz.pl/karolina-szydlo-slabicka/stomatolog/gdynia#address-id=%5B966566%5D&filters%5Bspecializations%5D%5B%5D=103/" + image_url, stream=True)
-            # with open('netflix.jpg', 'wb') as handler:
-            #     handler.write(img_data)
+            if len(address_split) == 2:
+                streetnamearr.append(address_split[0])
+                citynamearr.append(address_split[1])
 
+            # Works for single image extraction
+            # image = doc.find("span", class_="mb-1 avatar avatar-circle no-background")
+            image_span = doc.find("span", class_="mb-1 avatar avatar-circle no-background")
+            image = image_span.find("img")
+            try:
+                image_url = "https:" + image['data-src'] #We need additional "https:" to complete the URL code
+            except Exception as e1:
+                image_url = "https:" + image['src']  # We need additional "https:" to complete the URL code
+
+            img_data = requests.get(image_url, stream=True)
+
+            # hdocimgname = docnamearr[-1] + ".jpg"
             if img_data.status_code == 200:  # 200 status code = OK
                 with open("netflix.jpg", 'wb') as f:
                     img_data.raw.decode_content = True
                     shutil.copyfileobj(img_data.raw, f)
 
-            img = mpimg.imread('netflix.jpg')
-            imgplot = plt.imshow(img)
-            plt.show()
-
-            if len(address_split) == 2:
-                streetnamearr.append(address_split[0])
-                citynamearr.append(address_split[1])
+            # # If you want to see the saved photo during the debugging
+            # img = mpimg.imread('netflix.jpg')
+            # imgplot = plt.imshow(img)
+            # plt.show()
 
         except Exception as e:
             pass
